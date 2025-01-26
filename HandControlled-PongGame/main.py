27b581444241a51ce7c1f1681ddabdc90ecf02d3
Manthan Awgan <manthanawgan@gmail.com>
2025-01-27 01:10:26 +0530

    solves some bugs

--- HandControlled-PongGame/main.py
+++ HandControlled-PongGame/main.py
@@ -7,34 +7,31 @@ mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 
 with mp_hands.Hands(
-    statis_image_mode = False,                      # Real-time tracking
-    max_num_hands = 2,                              #max number of hands
+    static_image_mode = False,                      # Real-time tracking
+    max_num_hands = 4,                              #max number of hands
     min_detection_confidence = 0.5,                 #minimum confidence for detection
     min_tracking_confidence = 0.5                   #minimum confidence for detection
 ) as hands:
 
     while cam.isOpened():
-        ref, frame = cam.read()
+        ret, frame = cam.read()
 
         cv2.imshow('frame feed',frame)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
         frame = cv2.flip(frame, 1)  
 
-        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBB) 
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) 
 
         results = hands.process(rgb_frame) 
 
 
-        if results.multi_hand_landmark:
-            for hand_landmark in results.multi_hand_landmark:
-                mp_drawing.draw_landmark(
+        if results.multi_hand_landmarks:
+            for hand_landmark in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(
                     frame, hand_landmark, mp_hands.HAND_CONNECTIONS
                 )
 
-        cv2.imshow(rgb_frame)
+        cv2.imshow('frame feed', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
